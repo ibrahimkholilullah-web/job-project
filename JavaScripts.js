7748368e1175f5289ca3b18179e18b553a875e15
ibrahimkholilullah-web <nijmehalext@gmail.com>
2025-02-05 00:11:27 +0600

    80 % Done Project

--- JavaScripts.js
+++ JavaScripts.js
@@ -34,3 +34,14 @@ function videos() {
     slider.style.transform = `translateX(-${scrollAmount}px)`;
 }
 
+document.addEventListener("DOMContentLoaded", function () {
+    const faqItems = document.querySelectorAll(".faq-item");
+
+    faqItems.forEach(item => {
+        const question = item.querySelector(".faq-question");
+
+        question.addEventListener("click", () => {
+            item.classList.toggle("active");
+        });
+    });
+});
