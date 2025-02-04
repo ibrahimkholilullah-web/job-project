7ec0d42dce7ba8a60bf2c22a57ef30b197a1b010
ibrahimkholilullah-web <nijmehalext@gmail.com>
2025-02-04 16:49:19 +0600

    sor video section add

--- JavaScripts.js
+++ JavaScripts.js
@@ -11,3 +11,26 @@ function slideNext() {
 
     slider.style.transform = `translateX(-${scrollAmount}px)`;
 }
+function slideNexts() {
+    const slider = document.querySelector('.video-sliders');
+    const cardWidth = document.querySelector('.video-cards').offsetWidth + 20; // Card width + margin
+    scrollAmount += cardWidth;
+    
+    if (scrollAmount >= slider.scrollWidth - slider.clientWidth) {
+        scrollAmount = 0; // Reset to first item if reaching the end
+    }
+
+    slider.style.transform = `translateX(-${scrollAmount}px)`;
+}
+function videos() {
+    const slider = document.querySelector('.videos')
+    const cardWidth = document.querySelector('.video').offsetWidth + 20; // Card width + margin
+    scrollAmount += cardWidth;
+    
+    if (scrollAmount >= slider.scrollWidth - slider.clientWidth) {
+        scrollAmount = 0; // Reset to first item if reaching the end
+    }
+
+    slider.style.transform = `translateX(-${scrollAmount}px)`;
+}
+
